2701512316f84da2b94e4b37aa806ce5ac762c75
Jura Šolja <jura.solja@ecx.io>
2019-05-06 08:47:03 +0200

    Added balance to view

--- src/main/java/com/foi/MyFinance/controller/TransactionController.java
+++ src/main/java/com/foi/MyFinance/controller/TransactionController.java
@@ -1,6 +1,8 @@
 package com.foi.MyFinance.controller;
 
+import com.foi.MyFinance.facade.UserFacade;
 import com.foi.MyFinance.model.TransactionModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,15 @@ public class TransactionController
     private static final String URL_TRANSACTION = "/user/transaction";
     private static final String VIEW_TRANSACTION = "transaction";
     private static final String MODEL_ATTRIBUTE_TRANSACTION_MODEL = "transactionModel";
+    private static final String MODEL_ATTRIBUTE_USER_BALANCE = "balance";
+
+    @Autowired
+    private UserFacade userFacade;
 
     @RequestMapping(value = URL_TRANSACTION, method = RequestMethod.GET)
     public String getViewTransaction(final Model model)
     {
+        model.addAttribute(MODEL_ATTRIBUTE_USER_BALANCE, userFacade.getUserModel().getBalance());
         model.addAttribute(MODEL_ATTRIBUTE_TRANSACTION_MODEL, new TransactionModel());
         return VIEW_TRANSACTION;
     }
